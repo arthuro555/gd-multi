e5b20bfefeae1dcde5196499c6142df088a4960a
arthuro555 <arthur.pacaud@hotmail.fr>
2020-07-14 22:28:46 +0200

    Make the tool a CLI to create command line emulated gdevelop games

---
+++ src/cli.js
@@ -0,0 +1,34 @@
+const gdexporter = require("gdexporter");
+const args = require("minimist")(process.argv.slice(2));
+const os = require("os");
+const fs = require("fs-extra");
+const path = require("path");
+
+let outputDir = args["o"]|| args["output"];
+let inputProject = args["p"] || args["project"];
+
+if(inputProject == undefined || outputDir == undefined) {
+    console.error("Input or output dir is missing!");
+    return;
+}
+let exportdir = os.tmpdir() + "/GD-MultiTemp";
+
+// Cleanup directory
+fs.removeSync(exportdir);
+fs.mkdirSync(exportdir);
+
+gdexporter.exporter(inputProject, exportdir).then(() => {
+    return gdexporter.loadProject(inputProject);
+})
+.then(project => {
+    fs.copyFileSync(path.join(__dirname, "templates", "index.js"), path.join(exportdir, "index.js"));
+    fs.writeFileSync(path.join(exportdir, "project.json"),
+        fs.readFileSync(path.join(__dirname, "templates", "package.json"))
+            .toString()
+            .replace("gamename", project.getName().toLowerCase().replace(" ", ""))
+            .replace("gameversion", project.getVersion())
+            .replace("gameauthor", project.getAuthor())
+    );
+    fs.copySync(exportdir, outputDir);
+    console.log("Done!");
+});
