9d88fc203c60842ff2825710448f1e5fd7f47c78
arthuro555 <arthur.pacaud@hotmail.fr>
2020-07-14 20:46:20 +0200

    Fix resource loading

--- Exported/index.js
+++ Exported/index.js
@@ -3,9 +3,14 @@ const fs = require("fs");
 const path = require("path");
 const repl = require('repl');
 const JSDOM = require("jsdom").JSDOM;
+const express = require("express");
+
+// We need to host the resources on a webserver. So we host one:
+const port = new express().use('/', express.static(__dirname)).listen(8000).address().port;
 
 // Make a virtual dom as similar to the browser as possible
 const jsdom = new JSDOM(undefined, {
+    url: "http://localhost:" + port + "/",
     runScripts: "dangerously", 
     resources: "usable", 
     pretendToBeVisual: true,
@@ -15,7 +20,7 @@ global.document = window.document;
 window.console = global.console;
 
 // Polyfill localStorage
-delete window.localStorage;
+delete window.localStorage; // Delete default implementation
 window.localStorage = new require('node-localstorage').LocalStorage(path.join(__dirname, "localStorage"));
 
 require('jsdom').JSDOM.fromFile(path.join(__dirname, "index.html")).then((jsdom) => {
@@ -35,38 +40,33 @@ require('jsdom').JSDOM.fromFile(path.join(__dirname, "index.html")).then((jsdom)
         }
         waitUntilGDLoad = () => {
             if(scriptsLoaded == scriptsToLoad) { resolver() } 
-            else{ 
-                console.log("Loading game... " + Math.floor((scriptsLoaded / scriptsToLoad) * 100))
-                setTimeout(waitUntilGDLoad, 200) 
-            }
+            else{ setTimeout(waitUntilGDLoad, 200) }
         }
         waitUntilGDLoad();
     });
 })
 .catch((r) => console.error(`Error while loading the game files: ${r}`))
 .then(() => {
-    // Load the game
-    //Initialization
-    var game = new window.gdjs.RuntimeGame(window.gdjs.projectData, {});
+    return new Promise((resolve) => {
+        //Initialization
+        var game = new window.gdjs.RuntimeGame(window.gdjs.projectData, {});
 
-    //Create a renderer
-    game.getRenderer().createStandardCanvas(document.body);
+        //Create a renderer
+        game.getRenderer().createStandardCanvas(document.body);
 
-    //Bind keyboards/mouse/touch events
-    game.getRenderer().bindStandardEvents(game.getInputManager(), window, document);
+        //Bind keyboards/mouse/touch events
+        game.getRenderer().bindStandardEvents(game.getInputManager(), window, document);
 
-    //Load all assets and start the game
-    game.loadAllAssets(function() {
-        game.startGameLoop();
-    }, function(percent) {
-        console.log(percent);
+        //Load all assets and start the game
+        game.loadAllAssets(function() {
+            game.startGameLoop();
+            resolve(game);
+        }, function(percent) {
+            window.console.log("GDevelop Loading Screen: Loading... " + percent + "% completed.");
+        });
     });
-
-    return game;
 })
 .then(game => {
     global.game = game;
     repl.start('> ').context.global = global;
 });
-
-//setInterval(() => { console.log("Still Alive"); });
